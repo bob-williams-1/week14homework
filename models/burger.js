b89034fbe720b7bf219b8d71b385e54de847de56
bob-williams-1 <bob.williams.dev@gmail.com>
2018-11-12 14:14:29 -0500

    burger.js changes

--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,15 @@
-// Import the ORM to create functions that will interact with the database.
+
 var orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
     orm.all("burger", function(res) {
-      cb(res);
+      cb(res)
     });
   },
-  // The variables cols and vals are arrays.
+
   create: function(cols, vals, cb) {
-    orm.create("burger", cols, vals, function(res) {
+    orm("burger", cols, vals, function(res) {
       cb(res);
     });
   },
@@ -18,7 +18,3 @@ var burger = {
       cb(res);
     });
   }
-};
-
-// Export the database functions for the controller (catsController.js).
-module.exports = burger;
