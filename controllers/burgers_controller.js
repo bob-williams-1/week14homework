6c2e23709f2450729e78fc17fc446af09966889c
bob-williams-1 <bob.williams.dev@gmail.com>
2018-11-12 14:13:26 -0500

    #5

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,11 +1,11 @@
-// Import models connection.
+
 var express = require("express");
 var router = express.Router();
 
 //IMPORT THE MODEL (burger.js) to use its database function --
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
+
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
@@ -26,10 +26,8 @@ router.post("/", function(req, res) {
   });
 });
 
-router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+
 
   burger.update({
     devoured: req.body.devour
