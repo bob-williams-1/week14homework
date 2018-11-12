c9380517c6e17155160811be795cd4a3043723a3
bob-williams-1 <bob.williams.dev@gmail.com>
2018-11-12 14:10:00 -0500

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,7 @@ var mysql = require("mysql");
 var connection; 
 // = mysql.createConnection({
 
-//   host: 'us-cdbr-iron-east-03.cleardb.net',
-//   user: 'b4b01c8243424c',
-//   password: '25e2b918',
-//   database: 'heroku_a21fea84d64bc4a'
-// });
+
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
